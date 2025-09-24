f03bf22e7595e41a34b9f688439cb7b4633434a1
Saba Morchilashvili <smorchilashvili@gmail.com>
2025-09-24 20:29:03 +0400

    🚀 accounting for homework 5 tasks - ✅ create 5 complex enums

---
+++ src/enums/ContractType.java
@@ -0,0 +1,21 @@
+package enums;
+
+public enum ContractType {
+
+    FULL_TIME(40),
+    PART_TIME(20),
+    INTERN(15),
+    FREELANCER(0),
+    CONSULTANT(10),
+    STRATEGIC_ALLIANCE(0);
+
+    private final int weeklyHours;
+
+    ContractType(int weeklyHours) {
+        this.weeklyHours = weeklyHours;
+    }
+
+    public int getWeeklyHours() {
+        return weeklyHours;
+    }
+}
---
+++ src/enums/Stack.java
@@ -0,0 +1,9 @@
+package enums;
+
+public enum Stack {
+    JAVA,
+    PYTHON,
+    KOTLIN,
+    JAVASCRIPT,
+    C_SHARP,
+}
