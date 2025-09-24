2830fbf0516527b7821ca97d720f1b4298621aeb
Saba Morchilashvili <smorchilashvili@gmail.com>
2025-09-24 20:33:26 +0400

    🚀 accounting for homework 5 tasks - ✅ create at least one record

--- src/company/meetingroom/MeetingRoomSession.java
+++ src/company/meetingroom/MeetingRoomSession.java
@@ -1,26 +1,26 @@
 package company.meetingroom;
 
+import enums.MeetingType;
+
 import java.time.LocalDateTime;
 
 import static utils.DateTimeUtils.*;
 
-public class MeetingRoomSession implements AutoCloseable {
+public record MeetingRoomSession(MeetingRoom room, LocalDateTime startTime, MeetingType meetingType) implements AutoCloseable {
 
-    private final MeetingRoom room;
-    private final LocalDateTime startTime;
-
-    public MeetingRoomSession(MeetingRoom room, LocalDateTime startTime) {
+    public MeetingRoomSession(MeetingRoom room, LocalDateTime startTime, MeetingType meetingType) {
         this.room = room;
         this.startTime = startTime;
-        System.out.printf("Meeting room - %s booked at %s", room.getRoomName(), formatTime(startTime));
+        this.meetingType = meetingType;
+        System.out.printf("Meeting room - %s booked at %s", room.roomName(), formatTime(startTime));
     }
 
     public void holdMeeting(String topic) {
-        System.out.printf("%nMeeting held in %s at %s. Reason: '%s'.%n", room.getRoomName(), formatTime(startTime), topic);
+        System.out.printf("%nMeeting held in %s at %s. Reason: '%s'.%n", room.roomName(), formatTime(startTime), topic);
     }
 
     @Override
     public void close() {
-        System.out.printf("Meeting room %s released.", room.getRoomName());
+        System.out.printf("Meeting room %s released.", room.roomName());
     }
 }
